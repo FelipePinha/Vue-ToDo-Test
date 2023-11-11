65484edf5e2741f2bd51790d47fccea119cc2936
Felipe Carvalho <felipe_pc7@hotmail.com>
2023-11-11 14:11:35 -0300

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import ItemList from './components/Items/Items.component.vue';
+
+const ItemsList: any = [
+    {
+        id: 1,
+        name: 'Coffee',
+        completed: false,
+    },
+    {
+        id: 2,
+        name: 'Books',
+        completed: false,
+    },
+];
+</script>
+
+<template>
+    <div>
+        <h1>My ToDo</h1>
+    </div>
+    <ItemList :Items="ItemsList"></ItemList>
+</template>
---
+++ src/components/Items/Items.component.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+defineProps<{ Items: any[] }>();
+</script>
+
+<template>
+    <div>Item List</div>
+    <ul v-for="data in Items" :key="data.id">
+        <li>{{ data.name }}</li>
+    </ul>
+</template>
