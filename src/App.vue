3ef023a105e173506e32ef91f31a1c525ef2ff83
Felipe Carvalho <felipe_pc7@hotmail.com>
2023-11-11 15:16:51 -0300

    complete todo

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts">
 import ItemList from './components/Items/Items.component.vue';
+import type { ItemInterface } from './models/Items/Item.interface';
+import { reactive, computed } from 'vue';
 
-const ItemsList: any = [
+const ItemsList: ItemInterface[] = reactive([
     {
         id: 1,
         name: 'Coffee',
@@ -12,12 +14,42 @@ const ItemsList: any = [
         name: 'Books',
         completed: false,
     },
-];
+    {
+        id: 3,
+        name: 'Notes',
+        completed: false,
+    },
+    {
+        id: 4,
+        name: 'Social',
+        completed: false,
+    },
+    {
+        id: 5,
+        name: 'Games',
+        completed: false,
+    },
+]);
+
+const completeTodo = (id: number) => {};
+
+const completedAll = computed(() => {
+    let all = ItemsList.length;
+    let countComplete = 0;
+
+    ItemsList.forEach((element: ItemInterface) => {
+        if (element.completed) {
+            countComplete++;
+        }
+    });
+    return countComplete == all ? '[ All todo is completed ]' : '[ No ]';
+});
 </script>
 
 <template>
     <div>
         <h1>My ToDo</h1>
+        <h3>{{ completedAll }}</h3>
     </div>
-    <ItemList :Items="ItemsList"></ItemList>
+    <ItemList :Items="ItemsList" @changeItem="completeTodo"></ItemList>
 </template>
--- src/components/Items/Items.component.vue
+++ src/components/Items/Items.component.vue
@@ -1,10 +1,21 @@
 <script setup lang="ts">
-defineProps<{ Items: any[] }>();
+import type { ItemInterface } from '../../models/Items/Item.interface';
+
+defineProps<{ Items: ItemInterface[] }>();
+
+const emit = defineEmits<{ (e: 'changeItem', id: number): any }>();
+
+const completeTodo = (id: number) => {
+    emit('changeItem', id);
+};
 </script>
 
 <template>
     <div>Item List</div>
     <ul v-for="data in Items" :key="data.id">
-        <li>{{ data.name }}</li>
+        <li>
+            {{ data.name }} -
+            <input type="checkbox" v-model="data.completed" @click="completeTodo(data.id)" />
+        </li>
     </ul>
 </template>
